e524965e7eda135d53ff371bfc177a532bc6d02a
GrantBirki <grantbirki@github.com>
2024-04-26 15:57:32 -0700

    remove unused method

--- src/models/project.cr
+++ src/models/project.cr
@@ -16,14 +16,4 @@ class Project
     event_handler = @events[event.type]
     event_handler.check_for_event if event_handler
   end
-
-  # this method is called at the end of the Project's class initialization
-  # it is used to construct all the events that the Project class has knowledge of by looking
-  # at the project's configuration
-  def construct_event_knowledge!
-    @config.events.each do |event_config|
-      event = EventRegistry.create_event(event_config.type, event_config)
-      @events[event_config.type] = event
-    end
-  end
 end
