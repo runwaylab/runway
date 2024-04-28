56b42e6e381f05b155eeebee1a1c337699c9034c
GrantBirki <grantbirki@github.com>
2024-04-27 18:56:53 -0700

    remove test case

--- src/lib/common.cr
+++ src/lib/common.cr
@@ -6,27 +6,8 @@ module Runway
     #   Example string: "*/5 * * * *" for every 5 minutes.
     # @return [Bool] `true` if the schedule is a cron schedule, `false` otherwise.
     protected def self.cron?(schedule : String) : Bool
-      banana?(schedule)
       cron_regex = /^((((\d+,)+\d+|(\d+(\/|-|#)\d+)|\d+L?|\*(\/\d+)?|L(-\d+)?|\?|[A-Z]{3}(-[A-Z]{3})?) ?){5,7})$|(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)/
       return cron_regex.match(schedule) != nil
-    end
-
-    def self.banana?(fruit : String) : Bool
-      if fruit == "banana"
-        puts "it is a banana"
-        return true
-      else
-        puts "it is not a banana"
-        return false
-      end
-
-      if fruit == "tomato"
-        puts "it is a tomato"
-        return true
-      else
-        puts "it is not a tomato"
-        return false
-      end
     end
   end
 end
