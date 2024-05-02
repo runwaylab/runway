8869be19d7c35928c43d84d343a623ad87678956
GrantBirki <grantbirki@github.com>
2024-05-02 15:01:28 -0700

    add spectator shard

---
+++ lib/spectator/src/spectator/formatting.cr
@@ -0,0 +1,7 @@
+require "./formatting/*"
+
+module Spectator
+  # Reports test results to the end-user in various formats.
+  module Formatting
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,7 +1,6 @@
-require "spec"
+require "../src/*"
+require "spectator"
 require "log"
-require "../src/runway/core/env"
-require "../src/runway/models/config"
 
 CONFIG = RunwayConfiguration.from_yaml(File.open("./config.yml"))
 EVENT  = CONFIG.projects.first.events.first
