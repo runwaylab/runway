32c53bb20d4247f037e375a96e516763f90870f4
GrantBirki <grantbirki@github.com>
2024-05-02 13:22:32 -0700

    improve unit test structure for the `common` file

--- src/runway/lib/common.cr
+++ src/runway/lib/common.cr
@@ -5,7 +5,7 @@ module Runway
     # @param schedule [String] The schedule to check.
     #   Example string: "*/5 * * * *" for every 5 minutes.
     # @return [Bool] `true` if the schedule is a cron schedule, `false` otherwise.
-    protected def self.cron?(schedule : String) : Bool
+    def self.cron?(schedule : String) : Bool
       cron_regex = /^((((\d+,)+\d+|(\d+(\/|-|#)\d+)|\d+L?|\*(\/\d+)?|L(-\d+)?|\?|[A-Z]{3}(-[A-Z]{3})?) ?){5,7})$|(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)/
       return cron_regex.match(schedule) != nil
     end
