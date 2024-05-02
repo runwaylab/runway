a91dd8b48b99e7de56f7484b039f40832ddb505e
GrantBirki <grantbirki@github.com>
2024-05-01 21:24:25 -0700

    use and check for artifacts

---
+++ spec/acceptance/wait-for-artifacts.cr
@@ -0,0 +1,39 @@
+# This file helps to "prime" or "boot" the acceptance test suite
+
+require "emoji"
+require "../../src/runway/lib/logger"
+
+log = RunwayLogger.setup_logger(ENV.fetch("LOG_LEVEL", "INFO").upcase)
+ACCEPTANCE_DIR = File.dirname(__FILE__)
+
+log.debug { "acceptance tests directory: #{ACCEPTANCE_DIR}" }
+log.info { Emoji.emojize(":mag: waiting for all artifacts to be created...") }
+
+ARTIFACTS = [
+  "#{ACCEPTANCE_DIR}/projects/project-1/result.txt",
+  "#{ACCEPTANCE_DIR}/logs/runway.log"
+]
+
+TOTAL_ATTEMPTS = 30
+SLEEP_DURATION = 1.second
+all_artifacts_ready = false
+
+TOTAL_ATTEMPTS.times do |attempt|
+  log.debug { "attempt ##{attempt + 1}" }
+
+  if ARTIFACTS.all? { |artifact| File.exists?(artifact) }
+    log.info { Emoji.emojize(":white_check_mark: all artifacts are ready!") }
+    all_artifacts_ready = true
+    break
+  end
+
+  missing_artifacts = ARTIFACTS.reject { |artifact| File.exists?(artifact) }
+  log.debug { "missing artifacts: #{missing_artifacts}" }
+
+  sleep SLEEP_DURATION
+end
+
+if !all_artifacts_ready
+  log.error { Emoji.emojize(":x: not all artifacts are ready!") }
+  exit 1
+end
