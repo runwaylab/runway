1e10f8a038aa7e7f7c1f5ddd68d2e8d8b8c47576
GrantBirki <grantbirki@github.com>
2024-05-10 12:27:33 -0700

    init release event

---
+++ src/runway/events/github_release.cr
@@ -0,0 +1,47 @@
+require "../models/base_event"
+require "../services/github"
+
+class GitHubRelease < BaseEvent
+  EventRegistry.register_event("github_release", self)
+  @repo : String
+
+  def initialize(log : Log, event : Event)
+    super(log, event)
+    @github = Runway::GitHub.new(log)
+    @repo = @event.repo.not_nil!
+    @timezone = Runway::TimeHelpers.timezone(@event.schedule.timezone)
+  end
+
+  # This method is called after the project's deployment has completed
+  # @param payload [Payload] the payload object that contains the deployment status and other information
+  # @return [Payload] the payload object that was passed in with possibly updated information/attributes
+  def post_deploy(payload : Payload) : Payload
+    @log.debug { "received a post_deploy() request for deployment.id: #{payload.id} from event.uuid: #{@event.uuid}" }
+    @log.debug { "post_deploy() payload: #{payload.inspect}" } if Runway::VERBOSE
+
+    # exit early if the payload doesn't have a run_post_deploy? attribute
+    return payload unless payload.run_post_deploy? == true
+
+    if payload.success?
+      @log.info { "deployment was successful" } unless Runway::QUIET
+    else
+      @log.error { "deployment failed" }
+    end
+
+    return payload
+  rescue error : Exception
+    @log.error { "error handling deployment event: #{error.message}" }
+    return payload
+  end
+
+  # Check for a GitHub release event for the specified repository
+  # This method uses post_deploy hooks to log information after a deployment completes/fails
+  def check_for_event : Payload
+    payload = Payload.new(ship_it: false, run_post_deploy: true)
+
+    @log.debug { "received a check_for_event() request for event.uuid: #{@event.uuid}" }
+    @log.info { "checking #{@repo} for a new release" } unless Runway::QUIET
+
+    return payload
+  end
+end
