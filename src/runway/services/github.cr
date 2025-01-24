344022cc0c9e7a34174c862881c1af652f73b6f1
GrantBirki <grant.birkinbine@gmail.com>
2025-01-23 21:21:10 -0800

    init github app logic

--- src/runway/services/github.cr
+++ src/runway/services/github.cr
@@ -1,10 +1,11 @@
 require "octokit"
+require "./github_app"
 require "../core/logger"
 
 module Runway
   class GitHub
     @miniumum_rate_limit : Int32
-    @client : Octokit::Client
+    @client : Octokit::Client | GitHubApp
 
     # The octokit class for interacting with GitHub's API
     # @param log [Log] the logger to use
@@ -93,21 +94,26 @@ module Runway
     # Creates an octokit.cr client with the given token (can be nil aka unauthenticated)
     # @param token [String?] the GitHub token to use for authentication - if nil, the client will be unauthenticated
     # @return [Octokit::Client] the client
-    protected def create_client(token : String?) : Octokit::Client
+    protected def create_client(token : String?) : Octokit::Client | GitHubApp
+      # octokit.cr wipes out the loggers, so we need to re-apply them... bleh
+      # fetch the current log level
+      log_level = @log.level
+      @log = Runway.setup_logger(log_level.to_s.upcase)
+
+      if ENV["GITHUB_APP_ID"]? && ENV["GITHUB_APP_INSTALLATION_ID"]? && ENV["GITHUB_APP_PRIVATE_KEY"]?
+        @log.info { "🔑 using github app authentication" } unless Runway::QUIET
+        return GitHubApp.new
+      end
+
       if (token.nil? || token.empty?) && ENV.fetch("SUPPRESS_STARTUP_WARNINGS", nil).nil?
         @log.warn { "No GitHub token provided. Please set the GITHUB_TOKEN environment variable to avoid excessive rate limiting." }
       end
 
-      # octokit.cr wipes out the loggers, so we need to re-apply them... bleh
-      # fetch the current log level
-      log_level = @log.level
-
       # create the client
+      @log.info { "🔑 using github token authentication" } unless Runway::QUIET
       client = Octokit::Client.new(access_token: token)
       client.auto_paginate = ENV.fetch("OCTOKIT_CR_AUTO_PAGINATE", "true") == "true"
       client.per_page = ENV.fetch("OCTOKIT_CR_PER_PAGE", "100").to_i
-
-      @log = Runway.setup_logger(log_level.to_s.upcase)
 
       return client
     end
